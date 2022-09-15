bc60ef224b1cec672d330095d22eed022500b867
Smriti Dhiman <smriti@Smritis-MacBook-Pro.local>
2022-09-14 18:12:57 -0600

    modified script

--- lab-1.py
+++ lab-1.py
@@ -1,6 +1,4 @@
 import requests
 
 response = requests.get('https://raw.githubusercontent.com/sdhiman00/CMPUT-404-Labs/main/lab-1.py')
-open('lab1.txt', 'wb').write(response.content)
-with open('/Users/smriti/Desktop/lab-1/lab1.txt', 'r') as f:
-    print(f.read())
+print(response.text)
